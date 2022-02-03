c44031172b062fbffb91b2b30bd688193ae87682
niltrix <axlgilby@gmail.com>
2022-02-04 02:32:18 +0900

    update...

--- script/desktop.sh
+++ script/desktop.sh
@@ -4,3 +4,5 @@
 #cp ../img/desktop.jpg ~/desktop.jpg
 #cp ../img/lock.jpg ~/lock.jpg
 dconf load / < ./desktop/desktop.dconf
+sudo apt update
+sudo apt install $(check-language-support)
--- script/fish.sh
+++ script/fish.sh
@@ -20,3 +20,4 @@ chsh -s /usr/bin/fish
 # install omf
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 
+stow --target=$HOME ../fish
