a9928d9cf45f7a3539cb7c81f03266c39fa08515
niltrix <axlgilby@gmail.com>
2022-02-08 01:10:19 +0900

    update kitty script

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
-    set -g theme_color_scheme nord
-    # Commands to run in interactive sessions can go here
+  # Commands to run in interactive sessions can go here
+  set -g theme_color_scheme nord
+  set -g direnv_fish_mode eval_on_arrow
 end
 
 # >>> conda initialize >>>
@@ -9,3 +10,7 @@ eval /home/bj/anaconda3/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
 
 fish_add_path /usr/local/go/bin
+if not functions -q __direnv_export_eval; and command -sq direnv
+  direnv hook fish | source
+  direnv export fish | source
+end
