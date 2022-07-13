cae008bfc97c37746663ba965fbe67030013c95b
niltrix <axlgilby@gmail.com>
2022-07-13 11:50:43 +0900

    updated..

--- script/docker.sh
+++ script/docker.sh
@@ -1,13 +1,28 @@
 #! /bin/bash
 
-sudo apt update
+DISTRO=$(grep '^ID_LIKE' /etc/os-release | sed 's/ID_LIKE=//')
+PM_UPDATE=
+PM_INSTALL=
+if [ "arch" = "$DISTRO" ]; then
+  echo "arch...use pacman"
+  PM_UPDATE="sudo pacman -Sy"
+  PM_INSTALL="sudo pacman -S --noconfirm"
+elif [ "ubutu" = "$DISTRO" ]; then
+  echo "arch...use apt"
+  PM_UPDATE="sudo apt update"
+  PM_INSTALL="sudo apt install -y"
+else
+  echo "$DISTRO is Not supported"
+fi
+
+$PM_UPDATE
 
 function install {
         which $1 &> /dev/null
 
         if [ $? -ne 0 ]; then
                 echo "Install: ${1}..."
-                sudo apt install -y $1
+                $PM_INSTALL $1
         else
                 echo "Already installed: ${1}"
         fi
@@ -24,7 +39,7 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt update
+$PM_UPDATE
 
 install docker-ce
 install docker-ce-cli
--- script/golang.sh
+++ script/golang.sh
@@ -1,14 +1,29 @@
 #! /bin/bash
 
 
-sudo apt update
+DISTRO=$(grep '^ID_LIKE' /etc/os-release | sed 's/ID_LIKE=//')
+PM_UPDATE=
+PM_INSTALL=
+if [ "arch" = "$DISTRO" ]; then
+  echo "arch...use pacman"
+  PM_UPDATE="sudo pacman -Sy"
+  PM_INSTALL="sudo pacman -S --noconfirm"
+elif [ "ubutu" = "$DISTRO" ]; then
+  echo "arch...use apt"
+  PM_UPDATE="sudo apt update"
+  PM_INSTALL="sudo apt install -y"
+else
+  echo "$DISTRO is Not supported"
+fi
+
+$PM_UPDATE
 
 function install {
         which $1 &> /dev/null
 
         if [ $? -ne 0 ]; then
                 echo "Install: ${1}..."
-                sudo apt install -y $1
+                $PM_INSTALL $1
         else
                 echo "Already installed: ${1}"
         fi
